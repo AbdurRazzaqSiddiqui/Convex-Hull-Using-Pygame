5ef69806a450e2dd0870e6f2626b918688d40fd9
Asad-U-Khan <122202257+Asad-U-Khan@users.noreply.github.com>
2023-11-19 23:02:12 +0500

    Add files via upload

--- line_intersect_func.py
+++ line_intersect_func.py
@@ -30,3 +30,40 @@ def line_intersection_checker_CCW(line1, line2):
     return False  # Lines do not intersect
 
 # Find intersection using Research
+def orientation(p, q, r):
+    val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+    if val == 0:
+        return 0  # Collinear
+    return 1 if val > 0 else 2  # Clockwise or Counterclockwise
+
+def on_segment(p, q, r):
+    return q[0] <= max(p[0], r[0]) and q[0] >= min(p[0], r[0]) and q[1] <= max(p[1], r[1]) and q[1] >= min(p[1], r[1])
+
+def do_intersect(line1, line2):
+    p1, q1 = line1
+    p2, q2 = line2
+
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
+
+    if o1 != o2 and o3 != o4:
+        return True
+
+    if o1 == 0 and on_segment(p1, p2, q1):
+        return True
+
+    if o2 == 0 and on_segment(p1, q2, q1):
+        return True
+
+    if o3 == 0 and on_segment(p2, p1, q2):
+        return True
+
+    if o4 == 0 and on_segment(p2, q1, q2):
+        return True
+
+    return False
+
+def line_intersection_checker_research(line1, line2):
+    return do_intersect(line1, line2)
